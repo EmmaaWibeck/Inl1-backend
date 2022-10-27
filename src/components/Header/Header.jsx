2b3a9621d78a9bb4813fc46d93077c3e943658f4
Emma Wibeck <Emmawibeck@gmail.com>
2022-10-27 15:08:37 +0200

    rensat upp

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='bg'>
+      <h3>Ärendehanteringssystem</h3>
+    </div>
+  )
+}
+
+export default Header
